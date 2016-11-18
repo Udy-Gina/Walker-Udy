01ef16285f2ed10afe4fe7c5689d945d99725295
lisawalker <lisawalker@lisas-imac>
2016-11-18 09:23:38 -0600

    InventoryType

--- PrincessBride/src/byui/cit260/princessBride/model/InventoryTypeView.java
+++ PrincessBride/src/byui/cit260/princessBride/model/InventoryTypeView.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.princessBride.control;
+package byui.cit260.princessBride.model;
 
 /**
  *
- * @author Gina Udy & Lisa Walker
+ * @author lisawalker & ginaudy
  */
+    
+ public enum InventoryTypeView {
+     ROPE, POTION, WATER   // items in game
+ }
 
- public class InventoryControl  {
 
-}
