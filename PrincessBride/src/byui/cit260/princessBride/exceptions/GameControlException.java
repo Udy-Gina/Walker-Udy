19609cec3a2a679b3efd72c6923014b49274f067
Gina Udy <Gina Udy@GinaSchool>
2016-11-26 18:32:54 -0500

    Updated GameControlException constructors.

--- PrincessBride/src/byui/cit260/princessBride/exceptions/GameControlException.java
+++ PrincessBride/src/byui/cit260/princessBride/exceptions/GameControlException.java
@@ -9,12 +9,25 @@ package byui.cit260.princessBride.exceptions;
  *
  * @author kellyjohnson
  */
-public class GameControlException {
+public class GameControlException extends Exception {
 
     public GameControlException() {
     }
 
-    
+    public GameControlException(String message) {
+        super(message);
+    }
 
-    
+    public GameControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public GameControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
